b111a081ef066eae59a6e69dfb85a8fb7688cd65
ecalifornica <queenin+git@gmail.com>
2015-04-05 00:10:03 -0700

    testing pygame channels

--- src/barf.py
+++ src/barf.py
@@ -4,13 +4,20 @@ import pygame
 
 arduino_conn = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
 
-pygame.mixer.pre_init(44100, -16, 2, 2048)
+pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.mixer.init()
 
-key_noise = pygame.mixer.Sound('/home/pi/mspf_audio/key_noise.wav')
-track_1 = pygame.mixer.Sound('/home/pi/tasty_waves.wav')
+key_channel = pygame.mixer.Channel(1)
+track_channel = pygame.mixer.Channel(2)
 
-while True:
+key_noise = pygame.mixer.Sound('/home/herb/mspf_audio/key_noise.wav')
+track_1 = pygame.mixer.Sound('/home/herb/mspf_audio/01.wav')
+
+
+key_channel.play(key_noise)
+track_channel.play(track_1)
+
+while False:
     key = arduino_conn.read()
     if key:
         key_noise.play()
