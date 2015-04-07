c1ada9687634bb355e955a1466b35dd1875eae2a
John Boiles <johnaboiles@gmail.com>
2015-04-06 23:52:24 -0700

    Scriptified the main script

--- mspf.py
+++ mspf.py
@@ -1,43 +1,70 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import pygame
+import os
+import random
 import sys
 import serial
-from string import ascii_uppercase
+from string import ascii_lowercase
 
-import pygame
 
-try:
-    arduino_conn = serial.Serial(port='/dev/ttyACM0',
-                                 baudrate=9600,
-                                 timeout=None)
-except:
-    # TO DO: handle this better, check for other tty devices.
-    print('No Arduino detected.')
-    sys.exit(1)
+def sounds_from_directory(directory):
+    filenames = [directory + filename for filename in os.listdir(directory) if filename.endswith(".wav")]
+    sounds = [pygame.mixer.Sound(filename) for filename in filenames]
+    return sounds, filenames
 
-pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=4096)
-pygame.mixer.init()
 
-keystroke_channel = pygame.mixer.Channel(1)
-track_channel = pygame.mixer.Channel(2)
-sound = pygame.mixer.Sound
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Accepts characters a-z and plays an audio file.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--loglevel', help='Set log level to DEBUG, INFO, WARNING, or ERROR', default='INFO')
+    parser.add_argument('--logfile', help='Log file to append to.',)
+    parser.add_argument('--uart', help='File descriptor of UART to connect to.', metavar="DEVICE[,BAUD]", default='/dev/tty.usbmodem836341')
+    parser.add_argument('--quote_dir', help='Directory containing quote wav files (must be 44.1kHz)', default='audio/quotes/')
+    parser.add_argument('--key_dir', help='Directory containing keystroke wav files (must be 44.1kHz)', default='audio/keys/')
 
-# The number of keys and audio files.
-key_count = 4
+    args = parser.parse_args()
+    log_level = args.loglevel
+    log_file = args.logfile
+    uart = args.uart
+    quote_dir = args.quote_dir
+    key_dir = args.key_dir
 
-keystroke_sound = sound('audio/key01.wav')
-track_list = [sound('audio/{}.wav'.format(i)) for i in range(key_count)]
-track_queue = []
+    numeric_log_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_log_level, int):
+        raise ValueError('Invalid log level: %s' % log_level)
+    logging.basicConfig(level=numeric_log_level, format='%(asctime)s %(levelname)s:%(message)s', filename=log_file)
 
-while True:
-    keystroke = arduino_conn.read(1)
-    if keystroke:
-        print(keystroke)
-        keystroke_channel.play(keystroke_sound)
-        track_number = ascii_uppercase.index(keystroke)
-        track = track_list[track_number]
-        if track not in track_queue:
-            track_queue.append(track)
+    # Parse the uart string and optional baud
+    if ',' in uart:
+        device, baud = uart.split(',')
+    else:
+        device = uart
+        baud = 115200
 
-    # If we have a queued track and are not already playing a track.
-    if track_queue and not track_channel.get_busy():
-        track = track_queue.pop(0)
-        track_channel.play(track)
+    try:
+        arduino_conn = serial.Serial(port=device, baudrate=baud, timeout=None)
+    except:
+        # TO DO: handle this better, check for other tty devices.
+        print('No Arduino detected on uart %s' % uart)
+        sys.exit(1)
+
+    pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
+
+    keystroke_channel = pygame.mixer.Channel(1)
+    quote_channel = pygame.mixer.Channel(2)
+
+    keystroke_sounds, keystroke_filenames = sounds_from_directory(key_dir)
+    quotes, quote_filenames = sounds_from_directory(quote_dir)
+
+    while True:
+        keystroke = arduino_conn.read()
+        if keystroke:
+            index = ascii_lowercase.index(keystroke)
+            keystroke_index = random.randint(0, len(keystroke_sounds) - 1)
+            keystroke_sound = keystroke_sounds[keystroke_index]
+            keystroke_channel.play(keystroke_sound)
+
+            print("Queueing quote %s" % quote_filenames[index % len(quotes)])
+            quote = quotes[index % len(quotes)]
+            quote_channel.queue(quote)
--- src/track.py
+++
@@ -1,8 +0,0 @@
-class Track(object):
-  """docstring for Track"""
-  def __init__(self, path):
-    super(Track, self).__init__()
-    self.path = path
-
-  def __eq__(self, other):
-    return self.path == other.path
